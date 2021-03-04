2fd3c312f0e8219132cd3eceb4c80aeffe243fd3
Theo Beers <theo.beers@fu-berlin.de>
2021-03-04 13:26:48 +0100

    Adjust a few styles

--- js/setFont.js
+++ js/setFont.js
@@ -5,7 +5,7 @@ function setFont() {
     document.documentElement.style.setProperty(
       "--arabic-font",
       // This is just a copy of the Bootstrap "native font stack"
-      "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'"
+      "system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', 'Liberation Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'"
     );
   } else {
     document.documentElement.style.setProperty("--arabic-font", selectedFont);
